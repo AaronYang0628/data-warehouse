ef35f289ab16fd6888e36037038921d4df35e7ef
AaronYang <aaron19940628@gmail.com>
2025-11-27 03:18:39 +0000

    build(helm): update image registry and tag for metadata operator
    - Update image registry from docker-registry.lab.zverse.space to harbor.zhejianglab.com
    - Change repository name to ay-dev/metadata-crd-operator
    - Bump image tag from v20250829r1 to v20251126
    - Update Flink job JAR URI to latest version (flink-es-ingest-job-1127.jar)

--- src/main/java/org/zhejianglab/astro/customresource/flink/FlinkJobConfig.java
+++ src/main/java/org/zhejianglab/astro/customresource/flink/FlinkJobConfig.java
@@ -97,7 +97,7 @@ public class FlinkJobConfig {
         .job(
             JobSpec.builder()
                 .jarURI(
-                    "http://data-and-computing.oss-cn-hangzhou-zjy-d01-a.res.cloud.zhejianglab.com/projects/slurm-on-k8s/intel-mpi-libs/flink-es-ingest-job-1.0.0-all.jar")
+                    "http://data-and-computing.oss-cn-hangzhou-zjy-d01-a.res.cloud.zhejianglab.com/projects/slurm-on-k8s/intel-mpi-libs/flink-es-ingest-job-1127.jar")
                 .parallelism(parallelism)
                 .upgradeMode(UpgradeMode.STATELESS)
                 .entryClass("com.zhejianglab.astronomy.metadata.file.MetadataExtractorJob")
